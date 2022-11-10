312eb0902373f84b9637f0f30224c6b0e678aa74
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-11-11 01:39:06 +0200

    client page successfull

--- convee-web01/Controllers/HomeController.cs
+++ convee-web01/Controllers/HomeController.cs
@@ -3,19 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using convee_web01.Models;
+using System.Data.Entity;
+using PagedList;
 
 namespace convee_web01.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private Riverside_HoldingsEntities db = new Riverside_HoldingsEntities();
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View();
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            ViewBag.CurrentSort = sortOrder;
+
+            //
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var clients = from c in db.CLIENTS select c;
+
+            //return results according to search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clients = db.CLIENTS.Where(c => c.NAME.Contains(searchString));
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            return View(clients.ToList().ToPagedList(pageNumber, pageSize));
         }
 
-        public ActionResult About()
+        public ActionResult Invoices(string id, int? page)
         {
-            ViewBag.Message = "Your application description page.";
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            //access products model
+            var invoicesVM = new ClientInvoicesVM();
+            invoicesVM.Client = db.CLIENTS.ToList();
+            invoicesVM.Invoices = db.INVOICES.ToList().ToPagedList(pageNumber, pageSize);
 
             return View();
         }
---
+++ convee-web01/Models/ClientInvoicesVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace convee_web01.Models
+{
+    public class ClientInvoicesVM
+    {
+        public PagedList.IPagedList<INVOICE> Invoices { get; set; }
+        public List<CLIENT> Client { get; set; }
+
+    }
+}
